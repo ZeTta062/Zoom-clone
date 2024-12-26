05225df67488262428de2ec01ae2eaa2cb629f04
ZeTta062 <samhak17@naver.com>
2024-12-26 13:08:20 +0900

    create room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,40 +1,24 @@
-const messageList = document.querySelector("ul");
-const idForm = document.querySelector("#ID");
-const messageForm = document.querySelector("#message");
+const frontSocket = io();
 
-const FrontSocket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room"); 
 
-function makeMessage(type, payload) {       // element > string
-    const msg = {type, payload};
-    return JSON.stringify(msg);
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`
 }
 
-
-FrontSocket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-} );
-
-FrontSocket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-    // console.log("New message: ", message.data);
-});
-
-FrontSocket.addEventListener("close", () => {
-    console.log("Disconnected to Server ❌");
-});
-
-messageForm.addEventListener("submit", (e) => {     // 메시지
-    e.preventDefault();
-    const input = messageForm.querySelector("input");
-    FrontSocket.send(makeMessage("new_message",input.value));      // 프런트 > 백엔드
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    frontSocket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
-})
-
-idForm.addEventListener("submit", (e) => {              // ID
-    e.preventDefault();
-    const input = idForm.querySelector("input");
-    FrontSocket.send(makeMessage("nickname",input.value));
-    input.value = "";
-})
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import { WebSocketServer } from 'ws';
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -10,34 +10,49 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); //CATCHALL URL
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
 
-const server = http.createServer(app);          //express
-const wss = new WebSocketServer({server});  //webSocket
+const httpServer = http.createServer(app);          //express
+const wsServer = SocketIO(httpServer);              //SocketIO server
+
+wsServer.on("connection", (BackSocket) => {
+    BackSocket.onAny((event) => {
+        console.log(`Socket Event:${event}`);
+    });
+    BackSocket.on("enter_room", (roomName, done) => {
+        // front에 있는 emit에 적은 function을 back에서 제어 할 수 있다
+        BackSocket.join(roomName);
+        done();
+    });
+});
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+httpServer.listen(3000, handleListen);
+
+
+
+/* const wss = new WebSocketServer({server});  //webSocket
 const BackSockets = [];
 
 wss.on("connection", (BackSocket) => {
     BackSockets.push(BackSocket);
-
+    
     console.log("Connected to Browser ✅");
     BackSocket["nickname"] = "Anon";                // 알수없는 사용자
     BackSocket.on ("close", () => {
         console.log("Disconnected to Browser ❌");
-    });
-
-    BackSocket.on("message", (msg) => {
-        const message = JSON.parse(msg);     // string > element
-        switch(message.type) {
-            case "new_message":      // 백엔드 > 프런트 메시지
+        });
+        
+        BackSocket.on("message", (msg) => {
+            const message = JSON.parse(msg);     // string > element
+            switch(message.type) {
+                case "new_message":      // 백엔드 > 프런트 메시지
                 BackSockets.forEach(aSocket => 
                     aSocket.send(`${BackSocket.nickname}: ${message.payload}`));
-                break;
-            case "nickname":           // 백엔드 > 프런트 ID
-                BackSocket["nickname"] = message.payload;
-                
-        }
-    });
-});
-
-server.listen(3000, handleListen);
-
+                    break;
+                    case "nickname":           // 백엔드 > 프런트 ID
+                    BackSocket["nickname"] = message.payload;
+                    
+                    }
+                    });
+                    });
+                    */
